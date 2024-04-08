5cefe2ec1b8f648bd7b987edf6ab619b3a9e4a5a
Alina <55455721+agonzalez22@users.noreply.github.com>
2024-04-08 16:08:17 -0400

    added vocab size

--- utils.py
+++ utils.py
@@ -3,8 +3,6 @@ Alina G and Jesse E
 Text
  """ 
 import numpy as np 
-import librosa
-import speech_recognition as sr
 import json
 from collections import Counter
 import math
@@ -15,41 +13,6 @@ import time
 SENTENCE_BEGIN = "<s>"
 SENTENCE_END = "</s>"
 UNK = "<UNK>"
-
-def load_wavs(pathname: str, files:list, get_labels=True) -> list: 
-    """ takes a list of paths leading to .wav files and reads them. Also gets 
-        the labels if needed from the titles. Pathname is the path the files
-        are in, assuming there is a folder. 
-    """
-    values = []
-    for file in files: 
-        if file.endswith('.wav'):
-            sig, rate = librosa.load(pathname + '/' + file, sr=None)
-            sig_rate = (sig.tolist(), rate)
-            code_lst = file.split('-') # splits the file_name up into fragments 
-            emotion_label = int(code_lst[2]) 
-            values.append((sig_rate, emotion_label)) 
-    return values 
-
-def get_audio_text(pathname:str, files:list) -> list: 
-    """ using pathname and list of files, attempts to convert audio to text
-    *** ASSIGNS 0 FOR AN UNRECOGNIZED AUDIO FILE***
-    """
-    texts = []
-    # intialize the recognizer...
-    r = sr.Recognizer()
-    for file in files: 
-        if file.endswith('.wav'): 
-            code_lst = file.split('-') # splits the file_name up into fragments 
-            emotion_label = int(code_lst[2]) 
-            with sr.AudioFile(pathname + '/' + file) as source:
-                audio = r.record(source)
-                try: 
-                    s = r.recognize_google(audio)
-                except: 
-                    s = 0 # in case our recognizer fails, we will just assign 0...
-                texts.append((s, emotion_label))
-    return texts
 
 def seperate_tups(lst:list) -> tuple: 
     """ takes in a list of tuple parings and returns a tuple of 2 lists """ 
